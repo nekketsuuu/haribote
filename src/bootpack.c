92cdfa3ec500deb70d55f5ab0ebaf9d946f6175b
nekketsuuu <nekketsuuu@users.noreply.github.com>
2018-07-01 00:44:04 +0900

    [day 04] use a pointer instead of `write_mem8`

--- src/bootpack.c
+++ src/bootpack.c
@@ -3,10 +3,11 @@ void write_mem8(int addr, int data);
 
 void HariMain(void)
 {
-	int i; /* 変数宣言。iという変数は、32ビットの整数型 */
+	int i;
+	char *p = (char *) 0xa0000;
 
-	for (i = 0xa0000; i <= 0xaffff; i++) {
-		write_mem8(i, i & 0x0f); /* MOV BYTE [i],15 */
+	for (i = 0; i <= 0xffff; i++) {
+		p[i] = i & 0x0f;
 	}
 
 	for (;;) {
--- src/naskfunc.nas
+++ src/naskfunc.nas
@@ -13,9 +13,3 @@
 _io_hlt:	; void io_hlt(void);
 		HLT
 		RET
-
-_write_mem8:	; void write_mem8(int addr, int data);
-		MOV		ECX,[ESP+4]		; [ESP+4]にaddrが入っているのでそれをECXに読み込む
-		MOV		AL,[ESP+8]		; [ESP+8]にdataが入っているのでそれをALに読み込む
-		MOV		[ECX],AL
-		RET
